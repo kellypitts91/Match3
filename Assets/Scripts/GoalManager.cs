8d6030999c9cbcdad3f338460f4d11ec27974cd4
Kelly Sharma <kellypitts91@yahoo.com>
2019-06-02 19:17:23 +1200

    Finished video 40 - Adding goal panels - has bug though due to new unity prefabs

---
+++ Assets/Scripts/GoalManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class BlankGoal {
+    public int numberNeeded;
+    public int numberColected;
+    public Sprite goalSprite;
+    public string matchValue;
+}
+
+public class GoalManager : MonoBehaviour
+{
+    public BlankGoal[] levelGoals;
+    public GameObject goalPrefab;
+    public GameObject goalIntroParent;
+    public GameObject goalGameParent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetupIntroGoals();
+    }
+
+    void SetupIntroGoals() {
+        for(int i = 0; i < levelGoals.Length; i++) {
+            //Create new goal panel at the goalIntroParent position
+            GameObject goal = Instantiate(goalPrefab, goalIntroParent.transform.position, Quaternion.identity);
+            goal.transform.SetParent(goalIntroParent.transform, false);
+            //Set the image and text of the goal
+            GoalPanel panel = goal.GetComponent<GoalPanel>();
+            
+            // for(int j = 0; j < goal.transform.childCount; j++) {
+            //     Transform currentItem = goal.transform.GetChild(j);
+            //     if(currentItem.CompareTag("goalImage")) {
+            //         panel.thisImage = currentItem.GetComponent<Image>();
+            //     }
+            //     Debug.Log(currentItem.name);
+            // }
+
+            Debug.Log(levelGoals[i].goalSprite);
+            Debug.Log(levelGoals[i].numberNeeded);
+            panel.thisSprite = levelGoals[i].goalSprite;
+            panel.thisString = "0/" + levelGoals[i].numberNeeded;
+
+            //Create new goal panel at the goalGameParent position
+            GameObject gameGoal = Instantiate(goalPrefab, goalGameParent.transform.position, Quaternion.identity);
+            gameGoal.transform.SetParent(goalGameParent.transform, false);
+
+            panel = gameGoal.GetComponent<GoalPanel>();
+            panel.thisSprite = levelGoals[i].goalSprite;
+            panel.thisString = "0/" + levelGoals[i].numberNeeded;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
